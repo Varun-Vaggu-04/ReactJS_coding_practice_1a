d8f7ff4db056bac216fa44cceb1c04f336e90a5f
Varun Vaggu <varunvaggu@gmail.com>
2024-07-11 00:35:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+    // Write your code here.
+    <div className='container'>
+      <h1 className='heading'>Super Over League</h1>
+  
+      <div className='image-container'>
+        <img
+          className='rcb-image'
+          src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+        />
+        <img
+          className='csk-image'
+          src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+        />
+      </div>
+    </div>
+  )
+  
+  ReactDOM.render(element, document.getElementById('root'))
+  
